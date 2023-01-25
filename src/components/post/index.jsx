a8c9d0f7c42a08b94ea92dcbdaf698ec3fa397a5
Marcos Willian <87791042+marcosmwx@users.noreply.github.com>
2023-01-25 17:27:09 -0300

    fix code 2

--- src/components/post/index.jsx
+++ src/components/post/index.jsx
@@ -1,16 +1,15 @@
-import React from 'React'
+import React from "react";
 
-const Calculados =({hipotenusa,catetoum,catetodois}) => {
-    return(
-        <div className='calculados'>
-            <div>
-                <h4 className='hipotenusa'>Hipotenusa = {hipotenusa}</h4>
-                <h4 className='catetoum'> Cateto oposto = {catetoum}</h4>
-                <h4 className='catetodois'>Cateto adjacente = {catetodois}</h4>
-            </div>
-            
-        </div>
-    )
-}
+const Calculados = ({ hipotenusa, catetoum, catetodois }) => {
+  return (
+    <div className="calculados">
+      <div>
+        <h4 className="hipotenusa">Hipotenusa = {hipotenusa}</h4>
+        <h4 className="catetoum"> Cateto oposto = {catetoum}</h4>
+        <h4 className="catetodois">Cateto adjacente = {catetodois}</h4>
+      </div>
+    </div>
+  );
+};
 
-export {Calculados}
+export { Calculados };
